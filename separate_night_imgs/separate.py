f6634cc59359b4237472d30affb3090538b6c9c7
mattias-westerberg <47081916+mattias-westerberg@users.noreply.github.com>
2019-02-26 20:52:07 +0100

    Improved efficiency and style of the script.

--- separate_night_imgs/separate.py
+++ separate_night_imgs/separate.py
@@ -5,7 +5,7 @@ import os
 input_path = 'images/images/'
 output_path = 'night/'
 file_type = '.jpg'
-break_point = 30
+break_point = 60
 sampling_rate = 10
 
 directory = os.fsencode(input_path)
@@ -16,17 +16,14 @@ for file in os.listdir(directory):
     if filename.endswith(file_type): 
 
         img = cv2.imread(input_path + filename, 1)
-        #img = cv2.resize(img, (400, 400))
         pixelvalue = 0
-        counter = 0
         
         for widthpixel in [x*sampling_rate for x in range(int(len(img)/sampling_rate)) if x*sampling_rate < len(img)]:
             for heightpixel in [y*sampling_rate for y in range(int(len(img[0])/sampling_rate)) if y*sampling_rate < len(img[0])]:
-                for colour in [0, 1, 2]:
+                for colour in range(len(img[0][0])):
                     pixelvalue = pixelvalue + img[widthpixel][heightpixel][colour]
-                    counter = counter + 1
 
-        mean = pixelvalue/counter
+        mean = pixelvalue / (len(img) * len(img[0]) * len(img[0][0]) / sampling_rate ** 2)
         if mean < break_point:
             cv2.imwrite(output_path + filename, img)
         continue
