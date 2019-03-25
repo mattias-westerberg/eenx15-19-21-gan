aee361e66e28f1dda5c8c837cd0c49e17099c986
Mattias Westerberg <mattiasweberg@gmail.com>
2019-03-25 14:28:18 +0100

    Fixed image loading and shaping

--- GAN/main.py
+++ GAN/main.py
@@ -2,25 +2,22 @@
 import numpy as np 
 import tensorflow as tf 
 import tensorboard
-import matplotlib.pyplot as plt 
-import pickle
+import matplotlib.pyplot as plt
 import tensorflow.keras.layers as layers
-import os
+import cv2
 
+
+IMAGE_WIDTH = 512
+IMAGE_HEIGHT = 512
 
 
 ''' Load image ''' 
 
-def load(image_file):
-  image = tf.io.read_file(image_file)
-  image = tf.image.decode_png(image, channels=3)
+def load(image_file):  
+  image = cv2.imread(image_file, cv2.IMREAD_COLOR)
+  image = cv2.resize(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
 
-  image = tf.cast(image, tf.float32)
-  
   return image
-
-# image_tensor = load_image('C:\mattiasGit\eenx15-19-21-gan\GAN\\test.png')
-# print(np.shape(image_tensor))
 
 
 ''' Create the generator ''' 
@@ -42,8 +39,7 @@ class Generator:
   def build_generator(self):
       # TODO:
       model = tf.keras.Sequential()
-      model.add(layers.Conv2D(256, (3, 3), activation = 'relu', input_shape=(48, 48, 3)))
-      model.add(layers.Dense(64, activation = 'softmax'))
+      model.add(layers.Reshape((IMAGE_HEIGHT, IMAGE_WIDTH, 3), input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3)))
 
       return model
 
@@ -87,66 +83,36 @@ class Discriminator:
         fake_loss = self.loss_fun(tf.zeros_like(fake_output), fake_output)
         self.loss = real_loss + fake_loss
 
-''' Load images for testing the program '''
-# day_image = load('C:\mattiasGit\eenx15-19-21-gan\GAN\\test.png')
-night_image = load('C:\mattiasGit\eenx15-19-21-gan\GAN\\night.jpg')
-real_image = load('C:\mattiasGit\eenx15-19-21-gan\GAN\\ground_truth.jpg')
 
 ''' Define optimizers '''
-generator_optimizer = tf.keras.optimizers.Adam(0.001)
+generator_optimizer = tf.keras.optimizers.Adam(0.0001)
+discriminator_optimizer = tf.keras.optimizers.Adam(0.0001)
 
-discriminator_optimizer = tf.keras.optimizers.Adam(0.001)
-
-''' Save checkpoints '''
 
 new_generator = Generator()
 test_generator = new_generator.build_generator()
-# test_generator.summary()
+
 new_discriminator = Discriminator()
 test_discriminator = new_discriminator.build_discriminator()
-print(np.shape(test_discriminator))
-
-print(np.shape(night_image))
-
-night_image = np.reshape(night_image/255.0, [-1, 48, 48, 3])
-# night_image = night_image/255.0
-
-generated_img = test_generator(night_image)
-
-print(np.shape(generated_img))
-
-plot_img = tf.cast(generated_img/255, tf.float32)
-plt.imshow(plot_img)
-plt.show()
 
 
+''' Save checkpoints '''
+#checkpoints_dir = './checkpoints'
+#checkpoints_prefix = os.path.join(checkpoints_dir, 'ckpt')
+#checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer, discriminator_optimizer=discriminator_optimizer, generator=new_generator.model, discriminator=new_discriminator.model)
 
-
-
-'''
-checkpoints_dir = './checkpoints'
-checkpoints_prefix = os.path.join(checkpoints_dir, 'ckpt')
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer, discriminator_optimizer=discriminator_optimizer,
-generator=new_generator.model, discriminator=new_discriminator.model)
-'''
 
 ''' Define the training loop '''
 EPOCHS = 100
 num_examples_to_generate = 1
 
-
-
-# plot_img = tf.cast(input_image/255, tf.float32)
-# plt.imshow(plot_img)
-# plt.show()
-
 # @tf.function
-def train_step(night_iamges, day_images):
+def train_step(night_images, day_images):
 
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
         generated_images = new_generator.model(day_images)
 
-        real_output = new_discriminator.model(night_iamges)
+        real_output = new_discriminator.model(night_images)
         fake_output = new_discriminator.model(generated_images)
 
         gen_loss = new_generator.loss_function(real_output, fake_output)
@@ -159,3 +125,14 @@ def train_step(night_iamges, day_images):
         discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, new_discriminator.trainable_variables))
 
 
+
+
+## TESTING THE PROGRAM ##
+night_image = load('./GAN/night.jpg')
+real_image = load('./GAN/ground_truth.jpg')
+
+night_image = np.reshape(night_image, (-1, IMAGE_HEIGHT, IMAGE_WIDTH, 3))
+generated_img = test_generator.predict(night_image)
+generated_img /= 255.0
+plt.imshow(generated_img[0])
+plt.show()
