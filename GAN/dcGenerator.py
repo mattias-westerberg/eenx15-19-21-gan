e5fb21bdf3a256bcbcfe1f6090edc42cde006f2d
Adam Wirehed <adam.wirehed@gmail.com>
2019-03-27 14:14:36 +0100

    Debugging
    Fixed colour channels and resolution

--- GAN/dcGenerator.py
+++ GAN/dcGenerator.py
@@ -14,17 +14,18 @@ class Generator:
 
         # Creating the model
         model = tf.keras.Sequential()
-        # Creating 7, 7, channels=256 layer
-        model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100,)))
+        model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100, 100, 3)))
         model.add(layers.BatchNormalization())  # Normalize the nodes values between 0-1 (instead of 0-255)
         model.add(layers.LeakyReLU())           # No negative values
 
+        # Creating 7, 7, channels=256 layer
         model.add(layers.Reshape((7, 7, 256)))
         # Simple assert to check resolution and color channels
         assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
 
         # Strides=(1, 1) to keep the resolution
         # This layer only changes the color channels from 256 -> 128
+        # Kernel size 5x5
         model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
         assert model.output_shape == (None, 7, 7, 128)
         model.add(layers.BatchNormalization())
@@ -39,7 +40,7 @@ class Generator:
 
         # Stride (2, 2) to double the size
         # Output Color channel=3 for RGB
-        model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+        model.add(layers.Conv2DTranspose(3, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
         assert model.output_shape == (None, 28, 28, 3)
 
         return self.model
