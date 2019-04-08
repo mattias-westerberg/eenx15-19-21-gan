35f30f96e4e9b082afae1287dc735dad6c82aaf1
Adam <adamsg93@hotmail.com>
2019-04-08 11:44:44 +0200

    Generator structure update
    - Added more channels to the depths of the even_generator.py
    - Moved around the images in tensorboard
    - Changed from tf.image.resize to tf.image.resize_images in gan.py

--- GAN/generators/even_generator.py
+++ GAN/generators/even_generator.py
@@ -12,28 +12,28 @@ class EvenGenerator(Generator):
             self.model = tf.keras.Sequential()
             self.model.add(tf.keras.layers.InputLayer(input_shape=(image_size, image_size, 3)))
 
-            self.model.add(tf.keras.layers.Conv2D(64, (3, 3), strides=(1, 1), padding='same'))
-            self.model.add(tf.keras.layers.LeakyReLU(alpha=0.8))
+            self.model.add(tf.keras.layers.Conv2D(256, (3, 3), strides=(1, 1), padding='same'))
+            self.model.add(tf.keras.layers.LeakyReLU(alpha=1.0))
+            self.model.add(tf.keras.layers.BatchNormalization())
+            
+            self.model.add(tf.keras.layers.Conv2D(128, (3, 3), strides=(1, 1), padding='same'))
+            self.model.add(tf.keras.layers.LeakyReLU(alpha=1.0))
+            self.model.add(tf.keras.layers.BatchNormalization())
+            
+            self.model.add(tf.keras.layers.Conv2D(96, (3, 3), strides=(1, 1), padding='same'))
+            self.model.add(tf.keras.layers.LeakyReLU(alpha=1.0))
             self.model.add(tf.keras.layers.BatchNormalization())
             
             self.model.add(tf.keras.layers.Conv2D(64, (3, 3), strides=(1, 1), padding='same'))
-            self.model.add(tf.keras.layers.LeakyReLU(alpha=0.8))
+            self.model.add(tf.keras.layers.LeakyReLU(alpha=1.0))
             self.model.add(tf.keras.layers.BatchNormalization())
             
-            self.model.add(tf.keras.layers.Conv2D(64, (3, 3), strides=(1, 1), padding='same'))
-            self.model.add(tf.keras.layers.LeakyReLU(alpha=0.8))
+            self.model.add(tf.keras.layers.Conv2D(32, (3, 3), strides=(1, 1), padding='same'))
+            self.model.add(tf.keras.layers.LeakyReLU(alpha=1.0))
             self.model.add(tf.keras.layers.BatchNormalization())
             
-            self.model.add(tf.keras.layers.Conv2D(64, (3, 3), strides=(1, 1), padding='same'))
-            self.model.add(tf.keras.layers.LeakyReLU(alpha=0.8))
-            self.model.add(tf.keras.layers.BatchNormalization())
-            
-            self.model.add(tf.keras.layers.Conv2D(64, (3, 3), strides=(1, 1), padding='same'))
-            self.model.add(tf.keras.layers.LeakyReLU(alpha=0.8))
-            self.model.add(tf.keras.layers.BatchNormalization())
-            
-            self.model.add(tf.keras.layers.Conv2D(64, (3, 3), strides=(1, 1), padding='same'))
-            self.model.add(tf.keras.layers.LeakyReLU(alpha=0.8))
+            self.model.add(tf.keras.layers.Conv2D(16, (3, 3), strides=(1, 1), padding='same'))
+            self.model.add(tf.keras.layers.LeakyReLU(alpha=1.0))
             self.model.add(tf.keras.layers.BatchNormalization())
 
             self.model.add(tf.keras.layers.Dense(3, activation='tanh'))
