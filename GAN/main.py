e627938e0187c8e6c764cb251ae2ea37ff721474
Mattias Westerberg <mattiasweberg@gmail.com>
2019-04-14 23:50:55 +0200

    Added VGG-19 Discriminator

--- GAN/main.py
+++ GAN/main.py
@@ -8,6 +8,7 @@ from networks.models.generator_FeedForward import FeedForwardGenerator
 from networks.models.generator_even import EvenGenerator
 from networks.models.discriminator_test import TestDisctriminator
 from networks.models.discriminator_tf import TFDisctriminator
+from networks.models.discriminator_vgg19 import Discriminator_VGG19
 
 # Disable some TF warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -26,11 +27,11 @@ flags = tf.app.flags
 flags.DEFINE_integer("epoch", 200, "Number of epochs to train [20]")
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate for adam optimiser [0.0002]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term for adam optimiser [0.5]")
-flags.DEFINE_integer("train_size", 1000, "The size of training images [np.inf]")
+flags.DEFINE_integer("train_size", 9652, "The size of training images [np.inf]")
 flags.DEFINE_integer("batch_size", 1, "The batch-size (number of images to train at once) [64]")
 flags.DEFINE_integer("image_size", 256, "The size of the images [n x n] [64]")
-flags.DEFINE_string("dataset_real", "tests/test_images_night", "Real dataset directory [tests/test_images_night].")
-flags.DEFINE_string("dataset_input", "tests/train_lisa_extension_20_12_crop_256.txt", "Input dataset directory [tests/test_images_day].")
+flags.DEFINE_string("dataset_real", "tests/nexar_night_c256", "Real dataset directory [tests/test_images_night].")
+flags.DEFINE_string("dataset_input", "tests/lisa_c256.txt", "Input dataset directory [tests/test_images_day].")
 flags.DEFINE_string("output_dir", "outputs", "Directory name to save the outputs of the model [outputs]")
 flags.DEFINE_string("input_transform", "resize", "How to pre-process the input images [resize]")
 flags.DEFINE_integer("sample_interval", 10, "Number of epochs between samples [16]")
@@ -38,15 +39,15 @@ flags.DEFINE_integer("sample_size", 16, "Number of samples to generate [16]")
 flags.DEFINE_integer("checkpoint_interval", 10, "Number of epochs between checkpoints [32]")
 FLAGS = flags.FLAGS
 
-#gan_even = GAN("EvenGAN", EvenGenerator(256), TestDisctriminator(256), image_size=256, c_dim=3, output_dir="outputs", bbox_weight=1.0, image_weight=1.0)
+#gan_even = GAN("EvenGAN", EvenGenerator(256), Discriminator_VGG19(256), image_size=256, c_dim=3, output_dir="outputs", bbox_weight=1.0, image_weight=1.0)
 #gan_cycle = GAN("CycleGAN", EvenGenerator(256), TestDisctriminator(256), image_size=256, c_dim=3, output_dir="outputs", bbox_weight=1.0, image_weight=1.0)
 #gan_auto_encoder = GAN("AutoEncoderGAN", EvenGenerator(256), TestDisctriminator(256), image_size=256, c_dim=3, output_dir="outputs", bbox_weight=1.0, image_weight=1.0)
 #gan_vgg19 = GAN("VGG19GAN", EvenGenerator(256), TestDisctriminator(256), image_size=256, c_dim=3, output_dir="outputs", bbox_weight=1.0, image_weight=1.0)
 
-gan_ff = GAN("FeedForwardGAN", FeedForwardGenerator(256), TFDisctriminator(256), image_size=256, c_dim=3, output_dir="outputs", bbox_weight=1.0, image_weight=1.0)
+gan_ff = GAN("FeedForwardGAN", FeedForwardGenerator(256), Discriminator_VGG19(256), image_size=256, c_dim=3, output_dir="outputs", bbox_weight=1.0, image_weight=1.0)
+
 
 with tf.Session(config=config) as sess:
-    
     gan_ff.train(sess, FLAGS)
     
     #gan_ff.infer(sess, "tests/test_inference", "inference_output", FLAGS)
@@ -54,4 +55,4 @@ with tf.Session(config=config) as sess:
     #FLAGS.checkpoint_interval = 100
     #FLAGS.batch_size = 4
     #FLAGS.learning_rate = 0.0001
-    #gan_ff.train_discriminator(sess, "tests/test_images_day", "tests/test_images_night", FLAGS)
+    #gan_ff.train_discriminator(sess, "tests/lisa_merged_c256", "tests/nexar_night_c256", FLAGS)
