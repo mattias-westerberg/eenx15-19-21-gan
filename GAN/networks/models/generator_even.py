a455ebc695548c7779e99be846fd709fef3a5a59
Adam Wirehed <adam.wirehed@gmail.com>
2019-04-16 08:54:17 +0200

    Added more layers to make a up-down structure in the even_gan

--- GAN/networks/models/generator_even.py
+++ GAN/networks/models/generator_even.py
@@ -14,10 +14,10 @@ class EvenGenerator(Generator):
     def __call__(self, image, is_training):
         with tf.variable_scope("generator"):
 
-            x = conv2d(image, 256, (3, 3), (1, 1), name='g_00_conv')
+            x = conv2d(image, 64, (3, 3), (1, 1), name='g_00_conv')
             x = lrelu(x, 0.8)
 
-            x = conv2d(x, 256, (3, 3), (1, 1), name='g_01_conv')
+            x = conv2d(x, 128, (3, 3), (1, 1), name='g_01_conv')
             x = self.bns[0](x, is_training)
             x = lrelu(x, 0.8)
 
@@ -25,20 +25,32 @@ class EvenGenerator(Generator):
             x = self.bns[1](x, is_training)
             x = lrelu(x, 0.8)
 
-            x = conv2d(x, 128, (3, 3), (1, 1), name='g_03_conv')
+            x = conv2d(x, 256, (3, 3), (1, 1), name='g_03_conv')
             x = self.bns[2](x, is_training)
             x = lrelu(x, 0.8)
 
-            x = conv2d(x, 128, (3, 3), (1, 1), name='g_04_conv')
+            x = conv2d(x, 256, (3, 3), (1, 1), name='g_04_conv')
             x = self.bns[3](x, is_training)
             x = lrelu(x, 0.8)
 
-            x = conv2d(x, 96, (3, 3), (1, 1), name='g_05_conv')
+            x = conv2d(x, 128, (3, 3), (1, 1), name='g_05_conv')
             x = self.bns[4](x, is_training)
             x = lrelu(x, 0.8)
 
-            x = conv2d(x, 64, (3, 3), (1, 1), name='g_06_conv')
+            x = conv2d(x, 128, (3, 3), (1, 1), name='g_06_conv')
             x = self.bns[5](x, is_training)
+            x = lrelu(x, 0.8)
+
+            x = conv2d(x, 128, (3, 3), (1, 1), name='g_07_conv')
+            x = self.bns[6](x, is_training)
+            x = lrelu(x, 0.8)
+
+            x = conv2d(x, 96, (3, 3), (1, 1), name='g_08_conv')
+            x = self.bns[7](x, is_training)
+            x = lrelu(x, 0.8)
+
+            x = conv2d(x, 64, (3, 3), (1, 1), name='g_09_conv')
+            x = self.bns[8](x, is_training)
             x = lrelu(x, 0.8)
 
             # Dense layer, kernel=(1, 1)
