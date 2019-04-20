34581c37ad66346889fa69ac25aae44114106c1f
Adam Wirehed <adam.wirehed@gmail.com>
2019-04-20 13:01:00 +0200

    Reverted back to the old structure for g_even + dense layer in the end

--- GAN/networks/models/generator_even.py
+++ GAN/networks/models/generator_even.py
@@ -17,27 +17,27 @@ class EvenGenerator(Generator):
         with tf.variable_scope("generator"):
             with tf.variable_scope("even"):
                 x = conv2d(image, 256, (3, 3), (1, 1), name='g_00_conv')
-                x = lrelu(x, 0.8)
+                x = lrelu(x, 1.0)
 
                 x = conv2d(x, 128, (3, 3), (1, 1), name='g_01_conv')
                 x = self.bns[0](x, is_training)
-                x = lrelu(x, 0.8)
+                x = lrelu(x, 1.0)
 
-                x = conv2d(x, 128, (3, 3), (1, 1), name='g_02_conv')
+                x = conv2d(x, 96, (3, 3), (1, 1), name='g_02_conv')
                 x = self.bns[1](x, is_training)
-                x = lrelu(x, 0.8)
+                x = lrelu(x, 1.0)
 
                 x = conv2d(x, 64, (3, 3), (1, 1), name='g_03_conv')
                 x = self.bns[2](x, is_training)
-                x = lrelu(x, 0.8)
+                x = lrelu(x, 1.0)
 
-                x = conv2d(x, 64, (3, 3), (1, 1), name='g_04_conv')
+                x = conv2d(x, 32, (3, 3), (1, 1), name='g_04_conv')
                 x = self.bns[3](x, is_training)
-                x = lrelu(x, 0.8)
+                x = lrelu(x, 1.0)
 
-                x = conv2d(x, 32, (3, 3), (1, 1), name='g_05_conv')
+                x = conv2d(x, 16, (3, 3), (1, 1), name='g_05_conv')
                 x = self.bns[4](x, is_training)
-                x = lrelu(x, 0.8)
+                x = lrelu(x, 1.0)
 
                 # Dense layer, kernel=(1, 1)
                 x = conv2d(x, 3, (1, 1), (1, 1), name='g_06_conv')
